1c3d2cb4196f31e54f76fc225d32f2c530720343
rominacostanzo <65253721+rominacostanzo@users.noreply.github.com>
2022-10-08 19:40:43 -0300

    se agrego eliminar producto

--- crud.js
+++ crud.js
@@ -63,20 +63,32 @@ function modificarProducto (posicion, nombre, precio){
 
 //modificarProducto (prompt("ingrese posicion a modificar"), prompt ("Ingrese nombre del producto"), prompt ("Agregue precio"))
 
-function Inicial (numero){ 
- if (numero==1){
+function eliminarProducto(posicion){
+    if(posicion > Productos.length+1) {
+        alert ("El valor ingresado no existe")
+    }
+    Productos.splice (posicion,1)
     mostrarListado (Productos);
- } else if (numero==2) {
-    agregarProducto (prompt("agregue el id del producto"), prompt("Especifique el producto"),( prompt("Defina precio")), Productos);
- } else if (numero==3){
-    mostrarProducto (prompt ("Ingrese la posicion numero...."), Productos)
- } else if (numero==4){
-    modificarProducto (prompt("ingrese posicion a modificar"), prompt ("Ingrese nombre del producto"), prompt ("Agregue precio"))
- }else (alert ("El valor ingresado es incorrecto"))
+}
+
+
+function Inicial (numero){ 
+ if (numero===1){
+    mostrarListado (Productos);
+ } else if (numero===2) {
+    agregarProducto (Number (Productos.length+1), prompt("Especifique el producto"),( Number (prompt("Defina precio"))), Productos);
+ } else if (numero===3){
+    mostrarProducto (Number (prompt ("Ingrese la posicion numero....")), Productos)
+ } else if (numero===4){
+    modificarProducto (Number (prompt("ingrese posicion a modificar")), prompt ("Ingrese nombre del producto"), Number (prompt ("Agregue precio")))
+ } else if (numero===5){
+    eliminarProducto (Number (prompt("Ingrese la posicion a eliminar")))
+ } else (alert ("El valor ingresado es incorrecto"))
  }
 
- Inicial (prompt(`Ingrese una de las siguientes opciones: 
+ Inicial (Number (prompt(`Ingrese una de las siguientes opciones: 
  1: Mostrar listado, 
  2: Agregar producto, 
  3: Mostrar producto, 
- 4; Modificar producto`))
+ 4: Modificar producto,
+ 5: Eliminar producto.`)))
